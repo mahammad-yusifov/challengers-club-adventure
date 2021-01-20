e3c29b05af948b61e0fef2c988185d021bf95c03
Mahammad Yusifov <52427732+Megative19@users.noreply.github.com>
2021-01-20 17:59:14 +0400

    some C# remembering

---
+++ MS-WarmUp/Easy/BreakingTheRecords/BreakingTheRecords.cs
@@ -0,0 +1,49 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+        int min=0,max=0;
+         int[] highAndLow = {scores[0],scores[0]};
+
+        foreach (int score in scores) {
+            if (score>highAndLow[0]){
+                highAndLow[0]=score;
+                max++;
+            }
+            if(score<highAndLow[1]){
+                highAndLow[1] =score;
+                min++;
+            }
+        }
+        return new int[]{max,min};
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] scores = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp))
+        ;
+        int[] result = breakingRecords(scores);
+
+        textWriter.WriteLine(string.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
